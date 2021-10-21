79d379292382a5eeb94edb9da198c6eb2ed816ea
kmngtkm <a5209032@gmail.com>
2021-10-21 20:57:37 +0900

    Fist Commit

---
+++ cgi-bin/vul.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import subprocess
+import cgi
+import io
+import sys
+
+# 文字化け対策
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+# POSTされたデータの取得
+form = cgi.FieldStorage()
+# inputタグのname='string'の入力値を取得
+string = form.getvalue('string')
+# コマンドの組み立て
+cmd = "echo " + str(string) + " | rev"
+# コマンドの実行
+vul = subprocess.run(cmd, shell=True, encoding='utf-8', stdout=subprocess.PIPE)
+
+# レスポンスヘッダの返却
+print('Content-type: text/html; charset=UTF-8')
+print('')
+# レスポンスボディの返却
+print(f'{string} -> {vul.stdout}')
