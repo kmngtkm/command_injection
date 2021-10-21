ba1e3aa2dd5743b55228968f124335497a066168
kmngtkm <a5209032@gmail.com>
2021-10-21 21:59:13 +0900

    脆弱性の解消

--- cgi-bin/vul.py
+++ cgi-bin/vul.py
@@ -12,12 +12,14 @@ form = cgi.FieldStorage()
 # inputタグのname='string'の入力値を取得
 string = form.getvalue('string')
 # コマンドの組み立て
-cmd = "echo " + str(string) + " | rev"
+cmd1 = ["echo", str(string)]
+cmd2 = "rev"
 # subprocessでコマンドの実行
-vul = subprocess.run(cmd, shell=True, encoding='utf-8', stdout=subprocess.PIPE)
+proc1 = subprocess.Popen(cmd1, stdout=subprocess.PIPE)
+proc2 = subprocess.Popen(cmd2, stdin=proc1.stdout)
 
 # レスポンスヘッダの返却
 print('Content-type: text/html; charset=UTF-8')
 print('')
 # レスポンスボディの返却
-print(f'{string} -> {vul.stdout}')
+print(f'{string} -> {proc2}')
